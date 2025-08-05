715a495f086f3e7073078c0075e10a4e6440d0a6
mowangshuying <1911364602@qq.com>
2025-08-06 05:27:42 +0800

    update

--- demo/json/demo.cpp
+++ demo/json/demo.cpp
@@ -1,5 +1,6 @@
 #include <nlohmann/json.hpp>
 #include <iostream>
+#include <vector>
 
 using json = nlohmann::json;
 
@@ -24,8 +25,32 @@ int main()
     std::cout << "object.currency:" << j.at("object").at("currency") << "\n";
 
     // json to str;
-    std::cout << "json to str:" <<j.dump() << "\n";
+    std::cout  << j.dump() << "\n";
 
     auto pj = json::parse(j.dump());
     std::cout << "pj.pi:" << pj.at("pi") << "\n";
+
+    json cj = { {"pi", 3.141}, {"happy", true}, {"name", "Niels"}, {"nothing", nullptr}, {"answer", {{"everything", 42}}}, {"list", {1, 0 ,2}}, {"object", {{"currency", "USD"}, {"value", 42.99}}} };
+    std::cout <<  cj.dump() << "\n";
+
+    std::vector<std::uint8_t> vBson = json::to_bson(cj);
+    json fj = json::from_bson(vBson);
+
+    // get;
+    auto pi = cj["pi"].get<int>();
+    std::cout << "pi:" << pi << std::endl;
+
+    // stl oper;
+    // std::cout << "stl oper demo:" << std::endl;
+
+    json animalArray = {"cat", "dog"};
+    animalArray.push_back("pig");
+    animalArray.push_back("duck");
+
+    std::cout << "array str:" << animalArray.dump() << std::endl;
+    
+    if (animalArray.is_array() && animalArray.size() > 0)
+        std::cout << "array size:" << animalArray.size() << std::endl;
+
+    std::cout << "animalArray lst:" << animalArray.at(animalArray.size() - 1) << std::endl;
 }
